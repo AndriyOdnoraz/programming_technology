5d41fea083a51b9a500faee161b91c91ef2dd418
Andrii Odnoraz <odnoraz.a@ajax.systems>
2025-06-07 21:41:39 +0300

    Test demo

---
+++ stm32l475/stm32_test/Debug/unit_test/test/test_Demo.c
@@ -0,0 +1,21 @@
+
+#ifdef TEST
+
+#include "unity.h"
+
+#include "Demo.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_Demo_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement Demo");
+}
+
+#endif // TEST
