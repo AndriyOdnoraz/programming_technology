0c130e155de31a25566a2e85f4facb6ce80935be
Andrii Odnoraz <odnoraz.a@ajax.systems>
2025-06-08 02:23:35 +0300

    New tests

--- stm32l475/stm32_test/Debug/unit_test/test/test_led.c
+++ stm32l475/stm32_test/Debug/unit_test/test/test_led.c
@@ -13,11 +13,25 @@ void tearDown(void)
 {
 }
 
+void test_led_init(void)
+{
+    LED_Pin_Reset_Expect();
+    LED_Init();
+}
+
 void test_led_on(void)
 {
     LED_Tick_get_IgnoreAndReturn(42);
     LED_SetMode(LED_ON);
     LED_Pin_Set_Expect();
+    LED_Update();
+}
+
+void test_led_off(void)
+{
+    LED_Tick_get_IgnoreAndReturn(42);
+    LED_SetMode(LED_OFF);
+    LED_Pin_Reset_Expect();
     LED_Update();
 }
 
